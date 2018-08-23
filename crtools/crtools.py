485d8f60d8805cafe7369f6534826b63595c27a3
Aaron Traas <aaron@traas.org>
2018-08-23 16:02:20 -0400

    Cleanups and further abstractions

--- crtools/crtools.py
+++ crtools/crtools.py
@@ -10,10 +10,23 @@ import json
 import os
 import pprint
 import requests
+import shutil
 import urllib
 
+def write_object_to_file(file_path, obj): 
+    """ Writes contents of object to file. If object is a string, write it 
+    directly. Otherwise, convert it to JSON first """
 
-def get_clan(api_key, clan_id, output_path):
+    # open file for UTF-8 output, and write contents of object to file
+    with codecs.open(file_path, 'w', 'utf-8') as f:
+        if isinstance(obj, str) or isinstance(obj, unicode):
+            string = obj
+        else:
+            string = json.dumps(obj, indent=4)
+        f.write(string)
+
+
+def get_clan(api_key, clan_id):
     """Grab clan data from API."""
 
     # curl -X GET --header 'Accept: application/json' --header "authorization: Bearer <API token>" 'https://api.clashroyale.com/v1/clans/%23JY8YVV'
@@ -25,13 +38,10 @@ def get_clan(api_key, clan_id, output_path):
     r = requests.get(url, headers=headers)
     clan = json.loads(r.text)
 
-    log_filename = os.path.join(output_path, 'clan.json')
-    with codecs.open(log_filename, 'w', 'utf-8') as f:
-        f.write(json.dumps(clan, indent=4, sort_keys=True))
 
     return clan
 
-def get_warlog(api_key, clan_id, output_path):
+def get_warlog(api_key, clan_id):
     """Grab war log data from API."""
 
     # curl -X GET --header 'Accept: application/json' --header "authorization: Bearer <API token>" 'https://api.clashroyale.com/v1/clans/%23JY8YVV/warlog'
@@ -43,45 +53,50 @@ def get_warlog(api_key, clan_id, output_path):
     r = requests.get(url, headers=headers)
     warlog = json.loads(r.text)
 
-    log_filename = os.path.join(output_path, 'warlog.json')
-    with codecs.open(log_filename, 'w', 'utf-8') as f:
-        f.write(json.dumps(warlog, indent=4, sort_keys=True))
-
     return warlog['items']
 
-
-def build_dashboard(api_key, clan_id, output_path):
+def render_dashboard(members, clan_name):
     """Render clan dashboard."""
-
-    log_path = os.path.join(output_path, 'log')
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-        os.makedirs(log_path)
-
-    clan = get_clan(api_key, clan_id, log_path)
-    warlog = get_warlog(api_key, clan_id, log_path)
-
-    members = clan['memberList']
-    clan_name = clan['name']
-    #pprint.pprint(clan['name']); return
-    #pprint.pprint(warlog)
-
-    member_headers = ['Rank', 'Name', 'Donations', 'Donations Recieved']
-    member_dash = []
-    for member in members:
-        member_dash.append([member["clanRank"], member["name"], member["donations"], member["donationsReceived"]])
-
-
     env = Environment(
         loader=PackageLoader('crtools', 'templates'),
         autoescape=select_autoescape(['html', 'xml'])
     )
 
-    template = env.get_template('dashboard.html.j2')
-    dashboard_html = template.render(members = member_dash, clan_name = clan_name, member_headers=member_headers)
+    template_vars = {
+        'members' : members, 
+        'clan_name' : clan_name, 
+        'member_headers' : ['Rank', 'Name', 'Donations', 'Donations Recieved']
+    }
 
-    dashboard_filename = os.path.join(output_path, 'index.html')
-    with codecs.open(dashboard_filename, 'w', 'utf-8') as f:
-        f.write(dashboard_html)
+    template = env.get_template('dashboard.html.j2')
+    return template.render(template_vars)
+
+def build_dashboard(api_key, clan_id, output_path):
+    """Compile and render clan dashboard."""
+
+    # remove output directory if previeously created to cleanup. Then 
+    # create output path and log path.
+    if os.path.exists(output_path):
+        shutil.rmtree(output_path)
+    log_path = os.path.join(output_path, 'log')
+    os.makedirs(output_path)
+    os.makedirs(log_path)
+
+    # Get clan data from API. Write to log.
+    clan = get_clan(api_key, clan_id)
+    write_object_to_file(os.path.join(log_path, 'clan.json'), json.dumps(clan, indent=4))
+
+    # Get war log data from API. Write to log.
+    warlog = get_warlog(api_key, clan_id)
+    write_object_to_file(os.path.join(log_path, 'warlog.json'), json.dumps(warlog, indent=4))
+
+    # grab importent fields from member list for dashboard
+    member_dash = []
+    for member in clan['memberList']:
+        member_dash.append([member['clanRank'], member['name'], member['donations'], member['donationsReceived']])
+
+    dashboard_html = render_dashboard(member_dash, clan['name'])
+    write_object_to_file(os.path.join(output_path, 'index.html'), dashboard_html)
  
+
 
