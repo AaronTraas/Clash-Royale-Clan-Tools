100d51df04c66fbef668b239efa45898c7655253
Aaron Traas <aaron@traas.org>
2018-12-28 12:54:04 -0500

    Nicely formatted war records per member.

--- crtools/crtools.py
+++ crtools/crtools.py
@@ -5,10 +5,11 @@ __license__   = 'LGPLv3'
 __docformat__ = 'reStructuredText'
 
 import codecs
+from datetime import datetime
+import dateutil.parser
 from jinja2 import Environment, PackageLoader, select_autoescape
 import json
 import os
-import pprint
 import requests
 import shutil
 import urllib
@@ -39,14 +40,13 @@ def get_clan(api_key, clan_id):
     r = requests.get(url, headers=headers)
     clan = json.loads(r.text)
 
-
     return clan
 
 def get_warlog(api_key, clan_id):
     """Grab war log data from API."""
 
     # curl -X GET --header 'Accept: application/json' --header "authorization: Bearer <API token>" 'https://api.clashroyale.com/v1/clans/%23JY8YVV/warlog'
-    url = 'https://api.clashroyale.com/v1/clans/' + urllib.parse.quote_plus(clan_id) + '/warlog'
+    url = 'https://api.clashroyale.com/v1/clans/' + urllib.parse.quote_plus(clan_id) + '/warlog?limit=8'
     headers = {
         'Accept': 'application/json',
         'authorization': 'Bearer ' + api_key
@@ -56,38 +56,21 @@ def get_warlog(api_key, clan_id):
 
     return warlog['items']
 
-def render_html_page(env, page_title, content, clan_name):
-    template = env.get_template('page.html.j2')
-    return template.render(
-            page_title = page_title,
-            content = content,
-            clan_name = clan_name
-        )
+def warlog_dates(warlog):
+    """ Return list of date strings from warlog. One entry per war. """
 
+    war_dates = []
+    for war in warlog:
+        war_dates.append(dateutil.parser.parse(war['createdDate']).strftime("%m-%d"))
+    return war_dates
 
-def render_dashboard(env, members, clan_name):
-    """Render clan dashboard."""
-
-    template_vars = {
-        'members' : members, 
-        'clan_name' : clan_name, 
-        'member_headers' : ['Rank', 'Name', 'Donations', 'Donations Recieved']
-    }
-
-    template = env.get_template('table.html.j2')
-    return render_html_page( 
-            env, 
-            page_title = "Dashboard for " + clan_name,
-            content    = template.render(template_vars),
-            clan_name  = clan_name
-        )
 
 def member_warlog(member_tag, warlog):
-    member_warlog = []
+    """ Return war participation records for a given member by member tag. """
 
+    member_warlog = []
     for war in warlog:
-        participation = {}
-        pprint.pprint(war)
+        participation = ''
         for member in war['participants']:
             if member['tag'] == member_tag:
                 participation = {
@@ -97,6 +80,34 @@ def member_warlog(member_tag, warlog):
                 }
         member_warlog.append(participation)
     return member_warlog
+
+def render_html_page(env, page_title, content, clan_name, clan_id):
+    template = env.get_template('page.html.j2')
+    return template.render(
+            page_title = page_title,
+            update_date = datetime.now().strftime('%c'),
+            content = content,
+            clan_name = clan_name,
+            clan_id    = clan_id
+        )
+
+def render_dashboard(env, members, clan_name, clan_id, war_dates):
+    """Render clan dashboard."""
+
+    template_vars = {
+        'members' : members, 
+        'clan_name' : clan_name, 
+        'war_dates' : war_dates
+    }
+
+    template = env.get_template('member-table.html.j2')
+    return render_html_page( 
+            env, 
+            page_title = "Dashboard for " + clan_name,
+            content    = template.render(template_vars),
+            clan_name  = clan_name,
+            clan_id    = clan_id
+        )
 
 def build_dashboard(api_key, clan_id, output_path):
     """Compile and render clan dashboard."""
@@ -123,7 +134,9 @@ def build_dashboard(api_key, clan_id, output_path):
     # grab importent fields from member list for dashboard
     member_dash = []
     for member in clan['memberList']:
-        member_row = [member['clanRank'], member['name'], member['donations'], member['donationsReceived']] + member_warlog(member['tag'], warlog)
+        #member_row = [member['clanRank'], member['name'], member['donations'], member['donationsReceived']] + member_warlog(member['tag'], warlog)
+        member_row = member
+        member_row['warlog'] = member_warlog(member['tag'], warlog)
         member_dash.append(member_row)
 
     env = Environment(
@@ -131,8 +144,6 @@ def build_dashboard(api_key, clan_id, output_path):
         autoescape=select_autoescape(['html', 'xml'])
     )
 
-    dashboard_html = render_dashboard(env, member_dash, clan['name'])
+    dashboard_html = render_dashboard(env, member_dash, clan['name'], clan['tag'], warlog_dates(warlog))
     write_object_to_file(os.path.join(output_path, 'index.html'), dashboard_html)
  
-
-
