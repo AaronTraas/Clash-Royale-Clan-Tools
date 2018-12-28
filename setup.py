48c16b5bbd4ea244cdf09f81e719efefbc0f791b
Aaron Traas <aaron@traas.org>
2018-12-28 14:13:38 -0500

    Install static assets

--- setup.py
+++ setup.py
@@ -67,6 +67,8 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['jinja2','configparser'],
 
+    include_package_data=True,
+
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
