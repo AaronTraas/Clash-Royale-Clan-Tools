22b6da800f191da852b676391d3cde569a9cdac6
Aaron Traas <aaron@traas.org>
2018-12-29 19:54:33 -0500

    CSS cleanups and better formatting
    Switched from Monseurat to Open Sans, mostly because of the condensed versions being available.
    Also killes `skeleton.css`. Was using skeleton as a base to get up and running, but I was using so little of it other than the base styles, and I modified it pretty heavily, so I decided to ditch it and just copy the styles I was using.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.1.0',
+    version='1.1.2',
 
     description='Python tools for creating a clan maagement dashboard for Clash Royale',
     long_description=long_description,
