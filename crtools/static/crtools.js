ecc1dc4aa5b877d2938d19ffd9fcfc7d19b9c3b1
Aaron Traas <aaron@traas.org>
2019-01-04 15:04:01 -0500

    Usability tweaks

--- crtools/static/crtools.js
+++ crtools/static/crtools.js
@@ -2,10 +2,14 @@ var war_participations = document.querySelectorAll('[data-tooltip]');
 
 war_participations.forEach(function(element) {
 	element.addEventListener('click', function(e) {
-		war_participations.forEach(function(element_remove) {
-			element_remove.classList.remove('show-tooltip');
-		});
-		element.classList.add('show-tooltip');
+		if( element.classList.contains('show-tooltip') ) {
+			element.classList.remove('show-tooltip');
+		} else {
+			war_participations.forEach(function(element_remove) {
+				element_remove.classList.remove('show-tooltip');
+			});
+			element.classList.add('show-tooltip');
+		}
 	});
 });
 
