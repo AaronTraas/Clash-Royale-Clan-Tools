ceb65967079a434b261180958f1c59c04c3a0cde
Aaron Traas <aaron@traas.org>
2019-01-09 14:55:15 -0500

    Added matrix that represents all arenas and all collection day battle wins.

--- crtools/crtools.py
+++ crtools/crtools.py
@@ -15,22 +15,34 @@ import tempfile
 from ._version import __version__
 
 ARENA_LEAGUE_LOOKUP = {
-    'League 1' : { 'id':  'arena-league-challenger-1' },
-    'League 2' : { 'id':  'arena-league-challenger-2' },
-    'League 3' : { 'id':  'arena-league-challenger-3' },
-    'League 4' : { 'id':  'arena-league-master-1' },
-    'League 5' : { 'id':  'arena-league-master-1' },
-    'League 6' : { 'id':  'arena-league-master-1' },
-    'League 7' : { 'id':  'arena-league-champion' },
-    'League 8' : { 'id':  'arena-league-grand-champion' },
-    'League 9' : { 'id':  'arena-league-ultimate-champion' }
+    'Arena 1'  : { 'id':  'arena-1',           'collection_win': { 'bronze': 40,  'silver': 80,  'gold': 140, 'legendary': 220 } },
+    'Arena 2'  : { 'id':  'arena-2',           'collection_win': { 'bronze': 50,  'silver': 100, 'gold': 175, 'legendary': 275 } },
+    'Arena 3'  : { 'id':  'arena-3',           'collection_win': { 'bronze': 60,  'silver': 120, 'gold': 210, 'legendary': 330 } },
+    'Arena 4'  : { 'id':  'arena-4',           'collection_win': { 'bronze': 70,  'silver': 140, 'gold': 245, 'legendary': 385 } },
+    'Arena 5'  : { 'id':  'arena-5',           'collection_win': { 'bronze': 80,  'silver': 160, 'gold': 280, 'legendary': 440 } },
+    'Arena 6'  : { 'id':  'arena-6',           'collection_win': { 'bronze': 90,  'silver': 180, 'gold': 315, 'legendary': 495 } },
+    'Arena 7'  : { 'id':  'arena-7',           'collection_win': { 'bronze': 100, 'silver': 200, 'gold': 350, 'legendary': 550 } },
+    'Arena 8'  : { 'id':  'arena-8',           'collection_win': { 'bronze': 110, 'silver': 220, 'gold': 385, 'legendary': 605 } },
+    'Arena 9'  : { 'id':  'arena-9',           'collection_win': { 'bronze': 120, 'silver': 240, 'gold': 420, 'legendary': 660 } },
+    'Arena 10' : { 'id':  'arena-10',          'collection_win': { 'bronze': 130, 'silver': 260, 'gold': 455, 'legendary': 715 } },
+    'Arena 11' : { 'id':  'arena-11',          'collection_win': { 'bronze': 140, 'silver': 280, 'gold': 490, 'legendary': 770 } },
+    'Arena 12' : { 'id':  'arena-12',          'collection_win': { 'bronze': 150, 'silver': 300, 'gold': 525, 'legendary': 825 } },
+    'League 1' : { 'id':  'challenger-1',      'collection_win': { 'bronze': 160, 'silver': 320, 'gold': 560, 'legendary': 880 } },
+    'League 2' : { 'id':  'challenger-2',      'collection_win': { 'bronze': 160, 'silver': 320, 'gold': 560, 'legendary': 880 } },
+    'League 3' : { 'id':  'challenger-3',      'collection_win': { 'bronze': 160, 'silver': 320, 'gold': 560, 'legendary': 880 } },
+    'League 4' : { 'id':  'master-1',          'collection_win': { 'bronze': 170, 'silver': 340, 'gold': 595, 'legendary': 935 } },
+    'League 5' : { 'id':  'master-2',          'collection_win': { 'bronze': 170, 'silver': 340, 'gold': 595, 'legendary': 935 } },
+    'League 6' : { 'id':  'master-3',          'collection_win': { 'bronze': 170, 'silver': 340, 'gold': 595, 'legendary': 935 } },
+    'League 7' : { 'id':  'champion',          'collection_win': { 'bronze': 180, 'silver': 360, 'gold': 630, 'legendary': 990 } },
+    'League 8' : { 'id':  'grand-champion',    'collection_win': { 'bronze': 180, 'silver': 360, 'gold': 630, 'legendary': 990 } },
+    'League 9' : { 'id':  'ultimate-champion', 'collection_win': { 'bronze': 180, 'silver': 360, 'gold': 630, 'legendary': 990 } }
 }
 
 WAR_LEAGUE_LOOKUP = {
-    0    : { 'id': 'war-league-bronze',    'name': 'Bronze League',    'loss':  80, 'win': 160 },
-    600  : { 'id': 'war-league-silver',    'name': 'Silver League',    'loss': 160, 'win': 320 },
-    1500 : { 'id': 'war-league-gold',      'name': 'Gold League',      'loss': 280, 'win': 560 },
-    3000 : { 'id': 'war-league-legendary', 'name': 'Legendary League', 'loss': 440, 'win': 880 }
+    0    : { 'id': 'bronze',    'name': 'Bronze League' },
+    600  : { 'id': 'silver',    'name': 'Silver League' },
+    1500 : { 'id': 'gold',      'name': 'Gold League' },
+    3000 : { 'id': 'legendary', 'name': 'Legendary League' }
 }
 
 def write_object_to_file(file_path, obj): 
@@ -54,8 +66,25 @@ def warlog_dates(warlog):
     return war_dates
 
 
-def member_warlog(member_tag, warlog, cardsPerCollectionBattleWin, config):
+def member_warlog(member, clan, warlog, config):
     """ Return war participation records for a given member by member tag. """
+    member_tag = member['tag']
+
+    league_lookup = ARENA_LEAGUE_LOOKUP[member['arena']['name']]
+    collection_win_lookup = league_lookup['collection_win']
+
+    cardsPerCollectionBattleWin = 0
+    for score, lookupTable in WAR_LEAGUE_LOOKUP.items():
+        if clan['clanWarTrophies'] >= score:
+            war_league_id = lookupTable['id']
+            cardsPerCollectionBattleWin = collection_win_lookup[war_league_id]
+
+            ###########################################################
+            # FIXME -- this shouldn't be here. We shouldn't be 
+            # modifying the clan object in this function. 
+            clan['war_league'] = war_league_id
+            clan['war_league_name'] = lookupTable['name']
+            ###########################################################
 
     member_warlog = []
     for war in warlog:
@@ -169,18 +198,6 @@ def process_members(clan, warlog, config):
     if days_from_donation_reset == 7:
         days_from_donation_reset = 0
 
-    cardsPerCollectionBattleWin = 0
-    for score, lookupTable in WAR_LEAGUE_LOOKUP.items():
-        if clan['clanWarTrophies'] >= score:
-            cardsPerCollectionBattleWin = lookupTable['win']
-
-            ###########################################################
-            # FIXME -- this shouldn't be here. We shouldn't be 
-            # modifying the clan object in this function. 
-            clan['war_league'] = lookupTable['id']
-            clan['war_league_name'] = lookupTable['name']
-            ###########################################################
-
     # grab importent fields from member list for dashboard
     members = clan['memberList']
     members_processed = []
@@ -200,7 +217,7 @@ def process_members(clan, warlog, config):
             member['donations_daily'] = member['donations']
 
         # get member warlog and add it to the record
-        member['warlog'] = member_warlog(member['tag'], warlog, cardsPerCollectionBattleWin, config)
+        member['warlog'] = member_warlog(member, clan, warlog, config)
 
         member['donation_score'] = donations_score(member, days_from_donation_reset, config)
 
