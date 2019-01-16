4848fa8af197e3878945ec7c7119493b454ac1b1
Aaron Traas <aaron@traas.org>
2019-01-16 10:23:19 -0500

    Tooltips actually fully work now.

--- crtools/static/crtools.js
+++ crtools/static/crtools.js
@@ -1,30 +1,49 @@
 Tooltip = function() {
-	document.querySelectorAll('[data-tooltip]').forEach(function(element) {
-	    element.addEventListener("mouseenter", function(e) {
+    document.querySelectorAll('[data-tooltip]').forEach(function(element) {
+        element.addEventListener('mouseenter', function(e) {
+            addTooltip(element, false);
+        });
 
-	        var child_tooltip = e.target.querySelector('.tooltip');
-	        var tooltip;
-	        if(child_tooltip) {
-				tooltip = child_tooltip.cloneNode(true);
-	        } else {
-	        	tooltip = document.createElement('div')
-	        	tooltip.innerHTML = e.target.getAttribute('data-tooltip');
-	        }
-	        tooltip.className = "b-tooltip ";
+        element.addEventListener('click', function(e) {
+        	clearTooltips(true);
+            addTooltip(element, true);
+        });
 
-	        document.body.appendChild(tooltip);
+        element.addEventListener('mouseleave', function(e) {
+        	clearTooltips(false);
+        });
+    })
 
-	        positionAt(e.target, tooltip);
-	    });
+    function addTooltip(element, persist) {
+        var child_tooltip = element.querySelector('.tooltip');
+        var tooltip;
+        if(child_tooltip) {
+            tooltip = child_tooltip.cloneNode(true);
+        } else {
+            tooltip = document.createElement('div')
+            tooltip.innerHTML = element.getAttribute('data-tooltip');
+        }
+        tooltip.className = 'b-tooltip';
+        tooltip.dataset.persist = persist;
 
-	    element.addEventListener("mouseleave", function(e) {
-	    	var tooltips = document.querySelector(".b-tooltip");
-	    	if(tooltips) {
-	    		document.body.removeChild(tooltips);
-	    	}
+        document.body.appendChild(tooltip);
 
-	    });
-	})
+        positionAt(element, tooltip);
+    }
+
+    function clearTooltips(force) {
+        var tooltips = document.querySelectorAll('.b-tooltip');
+
+        if(tooltips) {
+        	tooltips.forEach( function(tooltip) {
+        		persist = (tooltip.dataset.persist=='true')
+        		console.log('persist, force', persist, force)
+        		if( !persist || force ) {
+		            document.body.removeChild(tooltip);
+        		}
+        	})
+        }
+    }
 
     /**
      * Positions the tooltip.
@@ -32,21 +51,21 @@ Tooltip = function() {
      * @param {object} tooltip - The tooltip itself.
      */
     function positionAt(parent, tooltip) {
-	    var dist  = 2;
+        var dist  = 2;
         var parentCoords = parent.getBoundingClientRect(), left, top;
 
-		left = parseInt(parentCoords.left) + ((parent.offsetWidth - tooltip.offsetWidth) / 2);
-		top = parseInt(parentCoords.top) - tooltip.offsetHeight - dist;
+        left = parseInt(parentCoords.left) + ((parent.offsetWidth - tooltip.offsetWidth) / 2);
+        top = parseInt(parentCoords.top) - tooltip.offsetHeight - dist;
 
-        tooltip.style.left = left + "px";
-        tooltip.style.top  = top + pageYOffset + "px";
+        tooltip.style.left = left + 'px';
+        tooltip.style.top  = top + pageYOffset + 'px';
     }
 };
 
 var filter_dropdown = document.getElementById('member-filter');
 var member_table = document.getElementById('member-table');
 filter_dropdown.addEventListener('change', function(e) {
-	member_table.dataset.filter = e.target.value;
+    member_table.dataset.filter = e.target.value;
 })
 
 var tooltip = new Tooltip();
