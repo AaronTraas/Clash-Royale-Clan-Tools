77c5078769542b44cec6f8d6a7c119b840e432cb
Aaron Traas <aaron@traas.org>
2019-10-02 11:48:04 -0400

    Updated pyroyale versions

--- clan.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-class ProcessedClan:
-	def __init__(self, config, ):
-		pass
--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
     setup_requires=['babel'],
-    install_requires=['jinja2','configparser','pyroyale>=1.0.1', 'requests', 'discord-webhook', 'google-api-python-client'],
+    install_requires=['jinja2','configparser','pyroyale>=1.0.3', 'requests', 'discord-webhook', 'google-api-python-client'],
 
     include_package_data=True,
 
