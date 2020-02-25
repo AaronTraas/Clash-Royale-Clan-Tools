d555762c7036a01b5c5e0077f667ffb020c9f95a
swedge <scott.wedge@gmail.com>
2020-02-25 13:15:03 -0500

    Fixes #54

--- crtools/crtools.py
+++ crtools/crtools.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-"""Tools for creating a clan maagement dashboard for Clash Royale."""
+"""Tools for creating a clan management dashboard for Clash Royale."""
 
 __license__   = 'LGPLv3'
 __docformat__ = 'reStructuredText'
